1eba1a1b8f0aa7be3764fb7b5b52853395ad9c37
jonathancesar21 <jonathan_cesar2015@hotmail.com>
2022-05-10 16:34:24 -0300

    nome do commit

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  
+
+    const [ pokemons, setPokemons ] = useState([]);
+
+    useEffect(() => {
+      fetch(`https://pokeapi.co/api/v2/pokemon`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        setPokemons(data.results)
+      })
+      
+    }, [])
+  
+    return (
+      <View>
+        <Text>Pokedex</Text>
+        <View>
+         <FlatList
+            horizontal={false}
+            data={pokemons}
+            keyExtractor={(pokemon) => pokemon.name}
+            renderItem={PokemonCards}
+            numColumns={3}
+         />
+        </View>
+      </View>
+    )
+  }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +45,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function PokemonCards(item){
+
+  //acessa o 'name' dentro do ITEM do OBJETO
+  const {name} = item.item
+  
+  const {index} = item
+
+  const num = index + 1;
+
+  return(
+    <View>
+      <Text>{index + 1}</Text>
+      <Image source={{uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ num + '.png'}}
+      style={{width: 100, height: 100, resizeMode: 'cover', margin: 15,}}
+      ></Image>
+      <Text>{name}</Text>
+    </View>
+  )
+}
